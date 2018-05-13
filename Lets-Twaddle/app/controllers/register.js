ca8082e40def83a7ca948593143e9c0da8c35178
mhooper93 <32500890+mhooper93@users.noreply.github.com>
2018-05-13 00:33:18 -0700

    controller logic for register,js

---
+++ Lets-Twaddle/app/controllers/register.js
@@ -0,0 +1,21 @@
+
+import Controller from '@ember/controller';
+import { match, not } from '@ember/object/computed';
+import { gte } from '@ember/object/computed';
+import { and } from '@ember/object/computed';
+import {computed} from '@ember/object'
+
+export default Controller.extend({
+  userEmail: '',
+  userPassword: '',
+  confirmedPassword: '',
+
+  isEmailValid: match('userEmail', /^.+@.+\..+$/),
+  isLongEnoughPw1: gte('userPassword.length', 8),
+  isLongEnoughPw2: gte('confirmedPassword.length', 8),
+  isPwMatch: computed(('userPassword', 'confirmedPassword'), function() {
+    return this.get('userPassword') == this.get('confirmedPassword')
+  }),
+  isDataOK: and('isEmailValid', 'isLongEnoughPw1', 'isLongEnoughPw2'),
+  isDisabled: not('isDataOK')
+});
